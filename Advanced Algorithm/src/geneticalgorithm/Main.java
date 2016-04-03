3d7ad7fda54c787d1de854dcd6e380478b7d54e3
hengfanz <franz_hengfanz@foxmail.com>
2016-04-03 16:02:24 +0800

    advanced algorithm 1.2

---
+++ Advanced Algorithm/src/geneticalgorithm/Main.java
@@ -0,0 +1,22 @@
+package geneticalgorithm;
+/**
+ * 
+ */
+
+/**
+ * @author Taoheng
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		new GA(4,10).run();
+		new TSP(30,6,10).run();
+	}
+
+}
