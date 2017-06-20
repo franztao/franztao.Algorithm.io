d9c7924daec5c24c309b84bcb5c47848ec50bc0f
hengfanz <franz_hengfanz@foxmail.com>
2017-06-20 23:24:10 +0800

    CSLS algorithm

---
+++ SRLG_Franz/src/head.h
@@ -0,0 +1,495 @@
+#ifndef __HEAD_H__
+#define __HEAD_H__
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include<map>
+#include <string.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<limits.h>
+#include<assert.h>
+#include "sys/timeb.h"
+
+#include <fstream>
+#include<sstream>
+
+#include <sys/time.h>
+#include "sys/timeb.h"
+
+#include<pthread.h>
+#include <signal.h>
+#include<semaphore.h>
+#include<errno.h>
+
+using namespace std;
+
+#ifndef __FranzDebug
+#define debug_showinitmatrix (false)
+#define debug_showsrlginfo (true)
+#else
+#define debug_showinitmatrix (false)
+#define debug_showdijkstrainfo (false)
+#endif // __FranzDebug
+
+const int MAX_NodeSize = (2015); //may be overflow
+
+#define algorithm_all 0
+#define algorithm_franz 1
+#define algorithm_COSE 2
+#define algorithm_IHKSP 3
+#define algorithm_ILP 4
+#define algorithm_IQP 5
+#define algorithm_ILP_sum 6
+#define algorithm_IQP_sum 7
+
+#define algorithm_getSRLGcsv -2
+#define algorithm_statisticParallelFranzAlgorithm -3
+
+#define ConsolePrint false
+
+#define exit_ILP_glp_set_obj_coef 50
+#define exit_pthread_create 51//ERROR; return code from pthread_create()
+
+#define LimitedTime 10 //algorithm's rumtime's limitaion.
+#define ThreadNum 1
+#define MAX_ITERATIONS 1000000 //KSP's the most larger iteration's number
+
+#define FranzMustNodeAlgorithmType 1 //1:internal dijistra 2:ILP
+
+const int WeightSort = (0); //0:weight value,1:hop
+const bool isUndirectedGraph = false; //the graph is or not directed
+
+//every srlg and what srlg is belonged from a edge.
+class SrlgMember {
+public:
+	//the corresponding id of every SRLG's members
+	vector<int> srlgMember;
+	//members' number of every SRLG
+	unsigned int srlgMembersNum;
+};
+
+class Edge {
+public:
+	int from; //start node
+	int to; //end node
+	int cost; //weight value
+	int index;
+	int id; //topo.csv. edge flag begin from 0.//logical sequence
+	int capacity; //capacity of network flow graph//could be removed
+	int ithsrlg; //SRLG id. -1:represent the edge do not belong any SRLG
+//	int ithsrlg4virtualedge; //SRLG's id of the edge which form the dummy vertex//could be removed
+	int revedgeid; //reverse edge's id -1:represent the graph is directed//could be removed
+	bool fsrlg, rsrlg;
+	Edge(int f, int t, int c, int index, int id, int cap, int iths, int r) {
+		this->from = f;
+		this->to = t;
+		this->cost = c;
+		this->index = index;
+		this->id = id;
+		this->capacity = cap;
+		this->ithsrlg = iths;
+//		this->ithsrlg4virtualedge = ithsrl;
+		this->revedgeid = r;
+		fsrlg = rsrlg = false;
+	}
+};
+
+class EdgeList {
+public:
+	vector<int> edgeList; //the list of every edge's id.
+};
+class Graph {
+private:
+	//all edges of graph.
+	vector<Edge> edges;
+public:
+	Edge& getithEdge(unsigned int i) {
+		return this->edges.at(i);
+	}
+	unsigned int getEdgeSize() {
+		return this->edges.size();
+	}
+	//demand.csv
+	int source, destination;
+
+	//set of all edge. topo.csv
+	//forward edge topological structure,row:vector column:vector
+	vector<EdgeList> ftopo_r_Node_c_EdgeList;
+	//reverse edge topological structure,row:vector column:vector
+	vector<EdgeList> rtopo_r_Node_c_EdgeList;
+
+	//edge number of initial graph
+	//node number of initial graph
+	unsigned int edgeNum, nodeNum;
+	//node number of initial graph which is transformed into no-star graph
+	unsigned int initNodeNumber_beforeTransToNostar;
+
+	//shared risk link groups,row:vector column: vector
+	vector<SrlgMember> srlgGroups;
+	unsigned int srlgGroupsNum;
+
+	//node's index map node's id
+	map<string, int> nindex_nid;
+	//node's  id map node's　index
+	map<int, string> nid_nindex;
+	//judge this node is valued to calculation,some nodes could be removed such as independent node.
+	//true :represent this node is valid
+	vector<bool> isValidNode;
+
+	vector<int> virtualNode;
+
+	Graph(int edgenum) {
+		source = destination = -1;
+		edgeNum = nodeNum = initNodeNumber_beforeTransToNostar = srlgGroupsNum =
+				0;
+		this->isValidNode = vector<bool>(edgenum * 3, false);
+	}
+
+	//transform the graph of star property into no-star property graph
+	void TransformToNostarGraph(void) {
+		int pre, next;
+		this->initNodeNumber_beforeTransToNostar = this->nodeNum;
+		//transform star edges belonging the same slrg into no star edge.
+		//A-->B A-->C ====> A->D->B A->D->C
+		for (unsigned int i = 0; i < this->srlgGroupsNum; i++) {
+			for (unsigned int j = 0; j < this->srlgGroups[i].srlgMember.size();
+					j++) {
+				pre = this->srlgGroups[i].srlgMember[j];
+				if (this->edges[pre].fsrlg)
+					continue;
+				int virtualnode = -1;
+				for (unsigned int k = j + 1;
+						k < this->srlgGroups[i].srlgMember.size(); k++) {
+					next = this->srlgGroups[i].srlgMember[k];
+					if (this->edges[pre].from == this->edges[next].from) {
+						if (-1 == virtualnode) {
+							virtualnode = this->nodeNum;
+							stringstream ss;
+							ss << virtualnode;
+							string str = "v" + ss.str();
+							this->nindex_nid.insert(
+									pair<string, int>(str, this->nodeNum));
+							this->nid_nindex.insert(
+									pair<int, string>(this->nodeNum, str));
+
+							this->isValidNode[this->nodeNum] = true;
+							this->nodeNum++;
+							this->virtualNode.push_back(this->edges[pre].from);
+
+						}
+						this->edges[next].from = virtualnode;
+						this->edges[next].fsrlg = true;
+					}
+				}
+				if (-1 != virtualnode) {
+					Edge e((this->edges[pre].from), virtualnode, 0,
+							(this->edges.size()), this->edgeNum, 1, i, -1);
+					this->edges[pre].from = virtualnode;
+					this->edges[pre].fsrlg = true;
+					//GraphList[inEdgeFlag].push_back(e);
+					this->edgeNum++;
+					this->edges.push_back(e);
+				}
+			}
+		}
+		for (unsigned int i = 0; i < this->srlgGroupsNum; i++) {
+			for (unsigned int j = 0; j < this->srlgGroups[i].srlgMember.size();
+					j++) {
+				pre = this->srlgGroups[i].srlgMember[j];
+				int virtualnode = -1;
+				if (this->edges[pre].rsrlg)
+					continue;
+				for (unsigned int k = j + 1;
+						k < this->srlgGroups[i].srlgMember.size(); k++) {
+					next = this->srlgGroups[i].srlgMember[k];
+					if (this->edges[pre].to == this->edges[next].to) {
+						if (-1 == virtualnode) {
+							virtualnode = this->nodeNum;
+//							this->nindex_nid[virtualnode] = this->nodeNum;
+//							this->nid_nindex[this->nodeNum] = virtualnode;
+							stringstream ss;
+							ss << virtualnode;
+							string str = "v" + ss.str();
+							this->nindex_nid.insert(
+									pair<string, int>(str, this->nodeNum));
+							this->nid_nindex.insert(
+									pair<int, string>(this->nodeNum, str));
+
+							this->isValidNode[this->nodeNum] = true;
+							this->nodeNum++;
+						}
+						this->edges[next].to = virtualnode;
+						this->edges[next].rsrlg = true;
+					}
+				}
+				if (-1 != virtualnode) {
+					//(inEdgeFlag,outEdgeflag,EdgeWeight,EdgeFlag,1,-1,-1,-1);
+					Edge e(virtualnode, (this->edges[pre].to), 0,
+							this->edges.size(), this->edgeNum, 1, i, -1);
+					this->edges[pre].to = virtualnode;
+					this->edges[pre].rsrlg = true;
+					this->edgeNum++;
+					this->edges.push_back(e);
+				}
+			}
+		}
+	}
+
+	//add edge into the topo structure.
+	void AddEdges(int EdgeFlag, int inEdgeFlag, int outEdgeflag,
+			int EdgeWeight) {
+		Edge fe(inEdgeFlag, outEdgeflag, EdgeWeight, EdgeFlag, this->edgeNum, 1,
+				-1, -1);
+
+		this->edgeNum++;
+		if (isUndirectedGraph) {
+			fe.revedgeid = this->edgeNum;
+		}
+		this->edges.push_back(fe);
+		if (isUndirectedGraph) {
+			Edge re(outEdgeflag, inEdgeFlag, EdgeWeight, EdgeFlag,
+					this->edgeNum, 1, -1, -1);
+			re.revedgeid = this->edgeNum - 1;
+			this->edgeNum++;
+			this->edges.push_back(re);
+		}
+	}
+
+	//construte the topo's form(ith node's edgelist)
+	void ConstructGraphbyLink(void) {
+		this->ftopo_r_Node_c_EdgeList = vector<EdgeList>(this->nodeNum);
+		this->rtopo_r_Node_c_EdgeList = vector<EdgeList>(this->nodeNum);
+		for (unsigned int i = 0; i < this->edges.size(); i++) {
+			this->ftopo_r_Node_c_EdgeList[this->edges.at(i).from].edgeList.push_back(
+					this->edges.at(i).id);
+			this->rtopo_r_Node_c_EdgeList[this->edges.at(i).to].edgeList.push_back(
+					this->edges.at(i).id);
+		}
+		return;
+	}
+
+	void InsertSRLGInfoToEdgeInfo() {
+		for (unsigned int i = 0; i < this->srlgGroupsNum; i++) {
+			SrlgMember srlgmem = this->srlgGroups.at(i);
+			for (unsigned int j = 0; j < srlgmem.srlgMembersNum; j++) {
+				int ithgroupithmem = srlgmem.srlgMember.at(j);
+				this->edges.at(ithgroupithmem).ithsrlg = i;
+			}
+		}
+	}
+
+	//some edge's srlg's id is -1, I let this edge have itself srlg'id.
+	void DesignAllEdgtoHaveSRLG() {
+		for (unsigned i = 0; i < this->edgeNum; i++) {
+			if (-1 == this->edges.at(i).ithsrlg) {
+				this->edges.at(i).ithsrlg = this->srlgGroupsNum;
+
+				SrlgMember srlgmem;
+				srlgmem.srlgMember.push_back(i);
+				if (isUndirectedGraph) {
+
+					int res = this->edges.at(i).revedgeid;
+					this->edges.at(res).ithsrlg = this->srlgGroupsNum;
+					srlgmem.srlgMember.push_back(res);
+				}
+				srlgmem.srlgMembersNum = srlgmem.srlgMember.size();
+				this->srlgGroups.push_back(srlgmem);
+				this->srlgGroupsNum++;
+
+			}
+		}
+	}
+
+	//resolve one edge is belonging mang srlgs.
+	void TransformedToEdgeBelongingOnlySRLG() {
+
+	}
+	//	//the initial graph's topological structure(matrix)
+	//	int InputGraphMatix[MAX_NodeSize][MAX_NodeSize];
+	//	int InputMaxGraphMatix[MAX_NodeSize][MAX_NodeSize];
+	//
+	//	int FlagofInputGraphEdgeMatrix[MAX_NodeSize][MAX_NodeSize];
+	//	int FlagofInputMaxGraphEdgeMatrix[MAX_NodeSize][MAX_NodeSize];
+};
+
+class InclusionExclusionSet {
+public:
+	int veclen;
+	//AP path must pass or not pass edges or srlgs.
+	vector<bool> Inclusion;
+	vector<bool> Exlusion;
+
+	InclusionExclusionSet(int len) {
+		this->Inclusion = vector<bool>(len, true);
+		this->Exlusion = vector<bool>(len, true);
+		this->veclen = len;
+	}
+	void clear() {
+		this->Inclusion.clear();
+		this->Exlusion.clear();
+		this->Inclusion = vector<bool>(veclen, true);
+		this->Exlusion = vector<bool>(veclen, true);
+	}
+};
+class Request {
+public:
+//	int source, destination;
+	vector<bool> APMustPassEdges;
+	vector<bool> APMustNotPassEdges;
+	vector<bool> BPMustNotPassEdges4AP;
+	vector<bool> BPMustNotPassEdgesRLAP;
+
+	vector<bool> APMustPassSRLGs;
+	vector<bool> APMustNotPassSRLGs;
+
+	vector<int> APExlusionEdges;
+	vector<int> APInclusionEdges;
+	vector<int> APSrlgs;	//may exist duplicate value
+
+	vector<int> RLAP_PathEdge;
+
+	//AP path information
+	vector<int> AP_PathNode;
+	vector<int> AP_PathEdge;
+	int APCostSum;
+	int APHopSum;
+
+	//BP path information
+	vector<int> BP_PathNode;
+	vector<int> BP_PathEdge;
+
+	int BPCostSum;
+
+	vector<int> edgeCapacity;// setting capacity of every edges of network flow graph
+
+	vector<bool> STNodeCut;	//true: belong s,false: belong t;
+	void clear(int edgenum) {
+		APHopSum = APCostSum = BPCostSum = 0;
+		this->AP_PathNode.clear();
+		this->AP_PathEdge.clear();
+		this->RLAP_PathEdge.clear();
+		for (int i = 0; i < edgenum; i++) {
+			this->APMustNotPassEdges[i] = true;
+			this->APMustPassEdges[i] = true;
+			this->BPMustNotPassEdges4AP[i] = true;
+			this->BPMustNotPassEdgesRLAP[i] = true;
+			this->edgeCapacity[i] = 1;
+		}
+	}
+	//initialize Request class.
+	Request(int s, int d, int edgenum) {
+		APHopSum = APCostSum = BPCostSum = 0;
+		this->APMustNotPassEdges = vector<bool>(edgenum, true);
+		this->APMustPassEdges = vector<bool>(edgenum, true);
+		this->BPMustNotPassEdges4AP = vector<bool>(edgenum, true);
+		this->BPMustNotPassEdgesRLAP = vector<bool>(edgenum, true);
+		this->edgeCapacity = vector<int>(edgenum, 1);
+	}
+
+	void initSRLG(int srlgnum) {
+		this->APMustPassSRLGs = vector<bool>(srlgnum, true);
+		this->APMustNotPassSRLGs = vector<bool>(srlgnum, true);
+	}
+};
+
+class Internal_Path {
+public:
+	Request *p_request;
+	vector<int> permute;
+};
+
+class DisjointPathPair {
+public:
+	//index of edge of AP path
+	vector<int> APedge;
+	//index of edge of BP path
+	vector<int> BPedge;
+	int APcostsum;
+	int BPcostsum;
+	unsigned int APhop;
+	unsigned int BPhop;
+	//true: can get the result.
+	bool SolutionNotFeasible;
+	//true: can get the optimal result.
+	bool SolutionisOptimalFeasible;
+
+	//true: the franz algorithm parralle
+	bool Isornot_Paralle;
+	//init class disjointpaths.
+	DisjointPathPair() {
+		this->APcostsum = INT_MAX;
+		this->BPcostsum = INT_MAX;
+		this->APhop = 0;
+		this->BPhop = 0;
+		this->SolutionNotFeasible = true;
+		this->Isornot_Paralle = false;
+		this->SolutionisOptimalFeasible = false;
+	}
+	//init class disjointpathpair,and define the length of APedge and BPedge.
+	DisjointPathPair(int aplen, int bplen) {
+		this->APcostsum = INT_MAX;
+		this->BPcostsum = INT_MAX;
+		this->APhop = 0;
+		this->BPhop = 0;
+		this->SolutionNotFeasible = true;
+		this->Isornot_Paralle = false;
+		this->SolutionisOptimalFeasible = false;
+		this->APedge = vector<int>(aplen);
+		this->BPedge = vector<int>(bplen);
+
+	}
+	//copy pathpair from dispath.
+	void copyResult(DisjointPathPair *dispath) {
+		this->APedge = vector<int>(dispath->APedge.size());
+		this->BPedge = vector<int>(dispath->BPedge.size());
+		std::copy(dispath->APedge.begin(), dispath->APedge.end(),
+				this->APedge.begin());
+		std::copy(dispath->BPedge.begin(), dispath->BPedge.end(),
+				this->BPedge.begin());
+		this->APcostsum = dispath->APcostsum;
+		this->BPcostsum = dispath->BPcostsum;
+//		this->APhop = 1 + this->APedge.size();
+//		this->BPhop = 1 + this->BPedge.size();
+		this->APhop = dispath->APhop;
+		this->BPhop = dispath->BPhop;
+		this->SolutionNotFeasible = dispath->SolutionNotFeasible;
+//		this->Isornot_Paralle = dispath->Isornot_Paralle;
+	}
+	//reinit the class disjointpathpair
+	void clearResult() {
+		this->APcostsum = this->BPcostsum = INT_MAX;
+		this->APhop = this->BPhop = 0;
+		this->SolutionNotFeasible = true;
+		this->Isornot_Paralle = false;
+		this->SolutionisOptimalFeasible = false;
+		this->APedge.clear();
+		this->BPedge.clear();
+	}
+
+};
+#endif
+
+/*
+ 1=<Weight value<=100
+ weight value is integer
+ No loop link
+ Multiple link with different weight
+ |vertices|<=2000,|outer degree|<=20,sparse graph
+ |Essential vertices|<=100
+ Directed loop graph
+ some tips modified the algorithm:
+ 0.reverse the whole graph
+ 1.water drop method
+ 2.every branch have the limited time
+ 3. if((-1!=vs_TSPDFS[j])&&(-1!=(dp.vsnode[j])))
+ 4.find 3 path method
+ 5.find the second path,add some value to the edge links the first path's enode
+ 6.add linkpaths between every enodes.
+ 7.optimate some array's long assignment problem
+ 8.first calculate the more enodesize's path
+ */
---
+++ SRLG_Franz/src/route.cpp
@@ -0,0 +1,302 @@
+#include "route.h"
+#include<iostream>
+#include "lib/lib_record.h"
+#include "lib/lib_time.h"
+#include "head.h"
+
+Graph *p_graph;
+DisjointPathPair *AlgorithmResult;
+//mutex lock for saving reslut
+pthread_mutex_t mutex_result;
+//pthread_mutex_t mutex_thread;
+int Algorithm;
+bool reverseDFS_mustedgeoutnode(Graph *p_graph, Request *p_request,
+		vector<int> &permute) {
+
+	typedef pair<int, int> P;
+	vector<int> dist((*p_graph).nodeNum, (-1));
+	vector<int> hop((*p_graph).nodeNum, (-1));
+	priority_queue<P, vector<P>, greater<P> > que;
+	unsigned int len;
+	dist[p_graph->destination] = 0;
+	hop[p_graph->destination] = 0;
+	que.push(P(0, p_graph->destination));
+
+	while (!que.empty()) {
+		P p = que.top();
+		que.pop();
+		int v = p.second;
+
+		if (dist[v] < p.first)
+			continue;
+		len = (*p_graph).rtopo_r_Node_c_EdgeList[v].edgeList.size();
+		for (unsigned int i = 0; i < len; i++) {
+
+			Edge &e = p_graph->getithEdge(
+					(*p_graph).rtopo_r_Node_c_EdgeList[v].edgeList[i]);
+
+			if ((!p_request->APMustNotPassEdges[e.id])) //|| (!p_request->APMustPassEdges[e.id]))
+				continue;
+			if (-1 == dist[e.from]) {
+				dist[e.from] = dist[v] + e.cost;
+				hop[e.from] = hop[v] + 1;
+				que.push(P(dist[e.from], e.from));
+			} else {
+				if (dist[e.from] > (dist[v] + e.cost)) {
+					dist[e.from] = dist[v] + e.cost;
+					hop[e.from] = hop[v] + 1;
+					que.push(P(dist[e.from], e.from));
+				} else {
+					if (dist[e.from] == (dist[v] + e.cost)) {
+						if (hop[e.from] > (hop[v] + 1)) {
+							hop[e.from] = hop[v] + 1;
+							que.push(P(dist[e.from], e.from));
+						}
+					}
+				}
+			}
+		}
+	}
+//
+//	cout << "*****************" << endl;
+//	for (unsigned i = 0; i < p_graph->edgeNum; i++) {
+//		if (!p_request->APMustPassEdges[i]) {
+//			cout << "must" << " "
+//					<< p_graph->nid_nindex[p_graph->getithEdge(i).from] << " "
+//					<< p_graph->nid_nindex[p_graph->getithEdge(i).to] << endl;
+//		}
+//
+//	}
+//	cout << endl;
+//	for (unsigned i = 0; i < p_graph->edgeNum; i++) {
+//
+//		if (!p_request->APMustNotPassEdges[i]) {
+//			cout << "must not " << " "
+//					<< p_graph->nid_nindex[p_graph->getithEdge(i).from] << " "
+//					<< p_graph->nid_nindex[p_graph->getithEdge(i).to] << endl;
+//		}
+//	}
+//
+//	cout << "-------------" << endl;
+
+	for (unsigned i = 0; i < permute.size(); i++) {
+		if (-1 == dist[p_graph->getithEdge(permute.at(i)).to]) {
+			return false;
+		}
+	}
+	return true;
+}
+
+//
+void printAnswer(DisjointPathPair *FranzAlgorithmResult, bool getanswer) {
+
+//	cout << endl
+//			<< "**************Successful to find disjoint path**************"
+//			<< endl;
+	if (!getanswer) {
+		cout << "APcost:" << "-1" << endl;
+		cout << "BPcost:" << "-1" << endl;
+		cout << "CostSum:" << "-1" << endl;
+		cout << "APhop:" << "-1" << endl;
+		cout << "BPhop:" << "-1" << endl;
+		cout << "HopSum:" << "-1" << endl;
+		return;
+	}
+	cout << "APcost:" << FranzAlgorithmResult->APcostsum << "  " << endl;
+	cout << "BPcost:" << FranzAlgorithmResult->BPcostsum << "  " << endl;
+	cout << "CostSum:"
+			<< (FranzAlgorithmResult->APcostsum
+					+ FranzAlgorithmResult->BPcostsum) << "  " << endl;
+	cout << "APhop:" << FranzAlgorithmResult->APhop << "  " << endl;
+	cout << "BPhop:" << FranzAlgorithmResult->BPhop << "  " << endl;
+	cout << "HopSum:"
+			<< (FranzAlgorithmResult->APhop + FranzAlgorithmResult->BPhop)
+			<< "  " << endl;
+	cout << "AP edge(" << (FranzAlgorithmResult->APhop - 1) << "): ";
+	unsigned int i = 0;
+	for (i = 0; i < FranzAlgorithmResult->APedge.size(); i++) {
+		record_result(WORK_PATH, FranzAlgorithmResult->APedge.at(i));
+		cout << FranzAlgorithmResult->APedge.at(i) << "   ";
+	}
+	cout << endl;
+
+	cout << "AP node(" << (FranzAlgorithmResult->APhop) << "): ";
+	for (i = 0; i < FranzAlgorithmResult->APedge.size(); i++) {
+
+		cout
+				<< p_graph->nid_nindex[p_graph->getithEdge(
+						FranzAlgorithmResult->APedge.at(i)).from] << "   ";
+
+	}
+	cout
+			<< p_graph->nid_nindex[p_graph->getithEdge(
+					FranzAlgorithmResult->APedge.at(i - 1)).to] << "   ";
+	cout << endl;
+
+	cout << "BP edge(" << (FranzAlgorithmResult->BPhop - 1) << "): ";
+	for (i = 0; i <= (FranzAlgorithmResult->BPedge.size() - 1); i++) {
+		record_result(BACK_PATH, FranzAlgorithmResult->BPedge.at(i));
+		cout << FranzAlgorithmResult->BPedge.at(i) << "   ";
+	}
+	cout << endl;
+	cout << "BP node(" << (FranzAlgorithmResult->BPhop) << "): ";
+	for (i = 0; i <= (FranzAlgorithmResult->BPedge.size() - 1); i++) {
+		cout
+				<< p_graph->nid_nindex[p_graph->getithEdge(
+						FranzAlgorithmResult->BPedge.at(i)).from] << "   ";
+	}
+
+	cout
+			<< p_graph->nid_nindex[p_graph->getithEdge(
+					FranzAlgorithmResult->BPedge.at(i - 1)).to] << "   ";
+	cout << endl << endl;
+
+}
+//verify whether the answer is really right.
+void verify_result(DisjointPathPair *FranzAlgorithmResult) {
+
+}
+
+void search_route(char *topo[MAX_EDGE_NUM], int edge_num,
+		char *demand[MAX_DEMAND_NUM], int demand_num, char *srlg[MAX_SRLG_NUM],
+		int srlg_num, int algorithm, char *str) {
+
+	Algorithm = algorithm;
+	//init result's class .
+	AlgorithmResult = new DisjointPathPair();
+	p_graph = new Graph(edge_num);
+	//load the graph's topological detailed information
+	if (!LoadGraphData(p_graph, topo, edge_num, demand, demand_num, srlg,
+			srlg_num)) {
+		printf("Error:LoadGraphData\n");
+		return;
+	}
+	DebugPrint(p_graph);
+
+	//franz
+	if ((algorithm_franz == algorithm) || (algorithm_all == algorithm)) {
+		AlgorithmResult->clearResult();
+		cout << endl << "--------------------------------------------" << endl;
+		print_time("");
+		if (FranzAlgorithmBasicFlows(p_graph)) {
+			printAnswer(AlgorithmResult, true);
+		} else {
+			printAnswer(AlgorithmResult, false);
+		}
+		if (AlgorithmResult->Isornot_Paralle)
+			cout << "isornot_parallar:" << 1 << endl;
+		else
+			cout << "isornot_parallar:" << 0 << endl;
+		print_time("FranzAlgorithmEnd");
+		cout << "--------------------------------------------" << endl << endl;
+	}
+
+	//COSE
+	if ((algorithm_COSE == algorithm) || (algorithm_all == algorithm)) {
+		AlgorithmResult->clearResult();
+		cout << endl << "--------------------------------------------" << endl;
+		print_time("");
+		if (CoSEAlgorithmBasicFlows(p_graph)) {
+			printAnswer(AlgorithmResult, true);
+		} else {
+			printAnswer(AlgorithmResult, false);
+		}
+		print_time("CoSEEnd");
+		cout << "--------------------------------------------" << endl << endl;
+	}
+
+	//KSP
+	if (((algorithm_IHKSP == algorithm) || (algorithm_all == algorithm))) {
+		AlgorithmResult->clearResult();
+		cout << endl << "--------------------------------------------" << endl;
+		print_time("");
+		if (KSPAlgorithmBasicFlows(p_graph)) { //ILPAlgorithmBasicFlows ILPAlgorithm_glpk
+			printAnswer(AlgorithmResult, true);
+		} else {
+			printAnswer(AlgorithmResult, false);
+		}
+		print_time("IHKSPEnd");
+		cout << "--------------------------------------------" << endl << endl;
+	}
+
+	//ILP
+	if ((algorithm_ILP == algorithm) || (algorithm_all == algorithm)) {
+		AlgorithmResult->clearResult();
+		cout << endl << "--------------------------------------------" << endl;
+		print_time("");
+		if (IPAlgorithmBasicFlows(p_graph, algorithm_ILP)) { // ILPAlgorithmBasicFlows   ILPAlgorithmBasicFlows_LocalSolver
+			printAnswer(AlgorithmResult, true);
+		} else {
+			printAnswer(AlgorithmResult, false);
+		}
+		print_time("ILPminEnd");
+		cout << "--------------------------------------------" << endl << endl;
+	}
+
+	//IQP
+	if ((algorithm_IQP == algorithm) || (algorithm_all == algorithm)) {
+		AlgorithmResult->clearResult();
+		cout << endl << "--------------------------------------------" << endl;
+		print_time("");
+		if (IPAlgorithmBasicFlows(p_graph, algorithm_IQP)) { // ILPAlgorithmBasicFlows   ILPAlgorithmBasicFlows_LocalSolver
+			printAnswer(AlgorithmResult, true);
+		} else {
+			printAnswer(AlgorithmResult, false);
+		}
+		print_time("IQPminEnd");
+		cout << "--------------------------------------------" << endl << endl;
+	}
+
+	//ILPsum
+	if ((algorithm_ILP_sum == algorithm) || (algorithm_all == algorithm)) {
+		AlgorithmResult->clearResult();
+		cout << endl << "--------------------------------------------" << endl;
+		print_time("");
+		if (IPAlgorithmBasicFlows(p_graph, algorithm_ILP_sum)) { // ILPAlgorithmBasicFlows   ILPAlgorithmBasicFlows_LocalSolver
+			printAnswer(AlgorithmResult, true);
+		} else {
+			printAnswer(AlgorithmResult, false);
+		}
+		print_time("ILPsumEnd");
+		cout << "--------------------------------------------" << endl << endl;
+	}
+	//IQPsum
+	if ((algorithm_IQP_sum == algorithm) || (algorithm_all == algorithm)) {
+		AlgorithmResult->clearResult();
+		cout << endl << "--------------------------------------------" << endl;
+		print_time("");
+		if (IPAlgorithmBasicFlows(p_graph, algorithm_IQP_sum)) { // ILPAlgorithmBasicFlows   ILPAlgorithmBasicFlows_LocalSolver
+			printAnswer(AlgorithmResult, true);
+		} else {
+			printAnswer(AlgorithmResult, false);
+		}
+		print_time("IQPsumEnd");
+		cout << "--------------------------------------------" << endl << endl;
+	}
+
+	//get srlg.csv file of three types from topo.csv file
+	if (algorithm_getSRLGcsv == algorithm) {
+		char abusolutepath[100];
+		int i;
+		int len = strlen(str);
+		for (i = 0; i < (len - 8); i++) {
+			abusolutepath[i] = str[i];
+		}
+		abusolutepath[i] = '\0';
+		getSRLGcsv(p_graph, abusolutepath);
+	}
+
+	if (algorithm_statisticParallelFranzAlgorithm == algorithm) {
+		//firstly,i would know that the case have solution,then judge the algorithm run the split-part.
+		AlgorithmResult->clearResult();
+		FranzAlgorithmBasicFlows(p_graph);
+		if (AlgorithmResult->Isornot_Paralle) {
+			cout << "parallel:1" << endl;
+		}
+	}
+
+	//verify_result(AlgorithmResult);
+	free(p_graph);
+	return;
+}
+
