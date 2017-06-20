99dda6d2a518183da3a3b8b4dcc13069965b1009
hengfanz <franz_hengfanz@foxmail.com>
2017-06-20 22:53:18 +0800

    python blog code

---
+++ franz-wab/handlers.py
@@ -0,0 +1,12 @@
+import re, time, json, logging, hashlib, base64, asyncio
+
+from coroweb import get, post
+from model import User, Comment, Blog, next_id
+
+@get('/')
+async def index(request):
+    users = await User.findAll()
+    return {
+            '__template__': 'test.html',
+            'users':users
+            }
