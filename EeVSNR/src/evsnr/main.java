a41c2d4bec84931d2b6f12fd0ee47a0680949a3b
hengfanz <franz_hengfanz@foxmail.com>
2017-07-07 23:32:59 +0800

    implement dynamic request arrival through timer mechanism, but the mechanism could not solve a critical problem that wait for a few time after embedding enhanced virtual network.
    Signed-off-by: hengfanz <franz_hengfanz@foxmail.com>

--- EeVSNR/src/evsnr/main.java
+++ EeVSNR/src/evsnr/main.java
@@ -63,46 +63,9 @@ public class main {
 		//
 		// // service parameter
 		// public int serviceNumber = 5;
-		Experiment exp=new Experiment(FDSubstrateNework,vnp);
+		Experiment exp = new Experiment(FDSubstrateNework, vnp);
 		exp.startExperiment();
-//		FDSubstrateNework.startExperiment(vnp);
 
-		// embedding virtual network into substrate network
-		// enhance the virtual network
-
-		// EnhancedVirtualNetwork EVNR1 = new EnhancedVirtualNetwork(7, 4, 4,
-		// VNR1);
-		// EVNR1.initSample1();
-		// EVNR1.computeItems();
-		// EVNR1.HeursitcAlgorithm4Survivability(4, EVNR1.FailureDependent);
-		// System.out.println("------------------" + "----------------");
-		//
-		// EnhancedVirtualNetwork EVNR2 = new EnhancedVirtualNetwork(7, 4, 4,
-		// VNR1);
-		// EVNR2.initSample1();
-		// EVNR2.computeItems();
-		// EVNR2.HeursitcAlgorithm4Survivability(4, EVNR2.FailureIndependent);
-		// System.out.println("------------------" + "----------------");
-
-		// VirtualNetwork VNR2 = new VirtualNetwork(2, 1, 2);
-		// VNR2.initSample2();
-		//
-		// EnhancedVirtualNetwork EVNR3 = new EnhancedVirtualNetwork(4, 2, 2,
-		// VNR2);
-		// EVNR3.initSample2();
-		// EVNR3.computeItems();
-		// EVNR3.HeursitcAlgorithm4Survivability(2, EVNR3.FailureDependent);
-		// System.out.println("------------------" + "----------------");
-
-		// VirtualNetworkRequest VNR3 = new VirtualNetworkRequest(1, 0, 2);
-		// VNR3.initSample3();
-		// EnhancedVirtualNetwork EVNR5 = new EnhancedVirtualNetwork(2, 0, 2,
-		// VNR3);
-		// EVNR5.initSample3();
-		// EVNR5.computeItems();
-		// EVNR5.OptimalAlgorithmIP4Survivability();
-		// EVNR5.HeursitcAlgorithm4Survivability(1, EVNR5.FailureDependent);
-		// System.out.println("------------------" + "----------------");
 
 	}
 
